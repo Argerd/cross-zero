f226405cb4152add7d407e6613d516f1d770a423
Argerd <argerd14@gmail.com>
2018-09-23 01:09:28 +0400

    Сырой пользовательский интерфейс

--- src/main/Logic.java
+++ src/main/Logic.java
@@ -1,5 +1,23 @@
 package main;
 
 public class Logic {
+    private boolean mIsX;
 
+
+
+    public boolean isX() {
+        return mIsX;
+    }
+
+    public void setX(boolean x) {
+        mIsX = x;
+    }
+
+    public Logic() {
+
+    }
+
+    private void start() {
+
+    }
 }
