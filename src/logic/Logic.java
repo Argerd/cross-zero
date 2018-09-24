39055ffe397fa46bf2a54bf5eaa7b3faf248433c
Argerd <argerd14@gmail.com>
2018-09-25 00:41:24 +0400

    К предыдущему коммиту

--- src/logic/Logic.java
+++ src/logic/Logic.java
@@ -2,21 +2,56 @@ package logic;
 
 public class Logic {
     // Сторона, за которую играет ПК
-    private String mSide = "x";
+    private String mSide = "X";
 
     // Поле игры
     private String[] mFieldMap = new String[9];
 
+    /** Счетчик ходов. */
     private int mTurnCounter = -1;
 
-    /**
-     * Ход ПК
-     *
-     * @param positionInMap - позиция на карте, куда сходит ПК
-     * @param side          - сторона, за которую играет ПК
-     */
-    public void turn(int positionInMap, String side) {
+    public String getSide() {
+        return mSide;
+    }
 
+    public String[] getFieldMap() {
+        return mFieldMap;
+    }
+
+    /** Очистка игрового поля для начала новой игры. */
+    public void cleanFieldMap() {
+        for (int i = 0; i < mFieldMap.length; i++) {
+            mFieldMap[i] = "";
+            mTurnCounter = -1;
+        }
+    }
+
+    /**
+     * Метод, анализирующий правильный ход для победы
+     *
+     * @return  - номер поля, куда нужно совершить ход для ПК
+     */
+    private int analysisOfVictory() {
+        int num = -1;
+
+        if (mSide == "X") {
+
+        } else {
+
+        }
+        return num;
+    }
+
+    /** Ход ПК. */
+    public void turn() {
+        switch (mTurnCounter) {
+            case -1:
+                mFieldMap[4] = mSide;
+                mTurnCounter++;
+                break;
+            case 0:
+
+        }
     }
 
 }
