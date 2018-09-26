258bb5451bf6a4d89c3e21dd48c481e2659bc713
Argerd <argerd14@gmail.com>
2018-09-26 23:23:54 +0400

    Удален ненужный код из класса Main в методе Main.

--- src/main/Main.java
+++ src/main/Main.java
@@ -15,27 +15,5 @@ public class Main {
 
     public static void main(String[] args) {
         new GUI();
-        State state = new State();
-        Logic logic = new Logic();
-
-        state.setFieldInMap(state.getSide(), (byte) 4);
-
-
-
-        state.setFieldInMap(getSideForButton(state.getSide()), (byte) 0);
-
-
-
-        state.incTurnCounter();
-        state.incTurnCounter();
-
-        System.out.println(state.getTurnCounter());
-
-        byte numberOfField = logic.analysisOfVictory(state.getSide(), state.getFieldMap(),
-                state.getTurnCounter());
-
-        System.out.println("итог " + numberOfField);
-
-
     }
 }
